b61fc2e5b0f8f4525f5023d704b61a3134385a2d
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-20 13:32:14 -0700

    create ClientsController and [GET] Create route

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Create(int id)
+    { 
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Specialty");
+      return View();
+    }
+  }
+}
