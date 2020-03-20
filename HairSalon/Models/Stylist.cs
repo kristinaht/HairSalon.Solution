bac73206c982af9060a5b24efaf225047c44d989
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-20 14:23:50 -0700

    attempt to display added clients

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 namespace HairSalon.Models
 {
   public class Stylist
-  {
-    public int StylistId { get; set; }
-    public string Specialty { get; set; }
-    public virtual ICollection<Client> Clients { get; set; }
-
+  { 
     public Stylist()
     {
       this.Clients = new HashSet<Client>();
     }
+    public int StylistId { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+
+   
   }
 }
