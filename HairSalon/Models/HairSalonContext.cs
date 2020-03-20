d3919e5edcfad50317512b566466122bc8747cc4
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-20 09:24:48 -0700

    create Client class

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,7 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    // public virtual DbSet<Stylist> Stylists { get; set; } 
+    public virtual DbSet<Stylist> Stylists { get; set; } 
 
     // public virtual DbSet<Client> Clients { get; set; }
 
