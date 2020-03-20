7d7475bbf8448bb49f2b6bbf05f6aeef01bb02be
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-20 09:56:52 -0700

    create Home Controller

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
   {
     public virtual DbSet<Stylist> Stylists { get; set; } 
 
-    // public virtual DbSet<Client> Clients { get; set; }
+    public virtual DbSet<Client> Clients { get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
   }
